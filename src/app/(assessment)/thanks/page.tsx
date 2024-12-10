aaaf9b3ae9aa08d19a08e987c80374d11b91ab36
Eric Avary <eavary@mac.com>
2024-12-10 00:22:23 -0800

    [assessment] thanks page

--- src/app/(assessment)/thanks/page.tsx
+++ src/app/(assessment)/thanks/page.tsx
@@ -1,6 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
 
 export default function AssessmentThanksPage() {
   return (
-    <h1>Assessment Thanks Page</h1>
+    <div className="flex flex-col spacing-y-8 my-20 items-center max-w-lg">
+      <Image
+        src="/icon-info.png"
+        alt="Info"
+        width={50}
+        height={50}
+      />
+      <h1 className="text-lg font-semibold my-3">Thank You</h1>
+      <p className="text-sm text-center font-semibold mb-3">
+        Your information was submitted to our team of immigration attorneys. Expect an email from hello@tryalma.ai.
+      </p>
+      <Link href="/" className="mt-6">
+        <Button className="px-16">
+          Go Back to Homepage
+        </Button>
+      </Link>
+    </div>
   )
 }
